77fa5a2ecbb7a2080988e447fd949549d08bd71c
yijoo <761610@naver.com>
2025-06-21 23:31:55 +0900

    projects

---
+++ tingleProject/projects/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from rest_framework.permissions import IsAuthenticated
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from rest_framework.viewsets import ModelViewSet
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class CommentViewSet(ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    
+    def get_queryset(self, **kwargs):
+        id = self.kwargs['post_id']
+        return self.queryset.filter(post=id)
